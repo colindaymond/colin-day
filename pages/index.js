8b71e73311bcb72594e3947eb986743cde748fcb
c01in <5925270+colindaymond@users.noreply.github.com>
2020-05-06 17:15:23 +0200

    darker grey

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ const Home = () => (
 
       main {
         padding: 5rem 0;
-        color: #333333;
+        color: #212121;
         flex: 1;
         display: flex;
         flex-direction: column;
