9ec2c12cfdf8592a9a7c90a2c610fe380f19879f
c01in <5925270+colindaymond@users.noreply.github.com>
2020-05-07 18:25:58 +0200

    line breaks

--- pages/posts/verse.js
+++ pages/posts/verse.js
@@ -14,9 +14,9 @@ export default function verse() {
 
           <h3>spring</h3>
           <p>
-          the snow melts.
-          water rushing between the trees.
-          pollen on the glass.
+          the snow melts\
+          water rushing between the trees\
+          pollen on the glass\
           up above, birdsong.
           </p>
 
