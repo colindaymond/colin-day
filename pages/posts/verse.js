5ad178afbf04e5ba3ee4ccba16962a00953a795b
c01in <5925270+colindaymond@users.noreply.github.com>
2020-05-07 16:34:39 +0200

    add to verse

---
+++ pages/posts/verse.js
@@ -0,0 +1,203 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>c01in</title>
+      <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/237/call-me-hand_emoji-modifier-fitzpatrick-type-3_1f919-1f3fc_1f3fc.png"></link>
+    </Head>
+
+    <main>
+      <h2 className="title">
+        c01in
+      </h2>
+
+      <p className="description">
+        words and pictures
+      </p>
+
+      <div className="grid">
+        <a href="https://www.venturing.io" target="_blank" className="card">
+          <h3>words &rarr;</h3>
+          <p>words from my blog, venturing</p>
+        </a>
+
+        <a href="https://www.venturing.io/photographs/" target="_blank" className="card">
+          <h3>pictures &rarr;</h3>
+          <p>souvenirs from the journey</p>
+        </a>
+
+        <a
+          href="https://www.youtube.com/watch?v=z9mjK8DYE3A&t=213s" target="_blank"
+          className="card"
+        >
+          <h3>moving pictures &rarr;</h3>
+          <p>on air: yangshuo, china, 2001</p>
+        </a>
+
+        <a
+          href="https://soundcloud.com/coldaymond" target="_blank"
+          className="card"
+        >
+          <h3>tunes &rarr;</h3>
+          <p>
+            music i like maybe you like too
+          </p>
+        </a>
+      </div>
+    </main>
+
+    <footer>
+        &#129305;&#127996;
+    </footer>
+
+    <style jsx>{`
+      .container {
+        min-height: 100vh;
+        padding: 0 0.5rem;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      main {
+        padding: 5rem 0;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      footer {
+        width: 100%;
+        height: 100px;
+        border-top: 1px solid #ff00e7;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      footer img {
+        margin-left: 0.5rem;
+      }
+
+      footer a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+
+      .title a {
+        color: #ff00e7;
+        text-decoration: none;
+      }
+
+      .title a:hover,
+      .title a:focus,
+      .title a:active {
+        text-decoration: underline;
+      }
+
+      .title {
+        color: #ff00e7;
+        margin: 0;
+        line-height: 1.15;
+        font-size: 3rem;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+      }
+
+      .title,
+      .description {
+        text-align: center;
+      }
+
+      .description {
+        line-height: 1.5;
+        font-size: 1.1rem;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+      }
+
+      code {
+        background: #fafafa;
+        border-radius: 5px;
+        padding: 0.75rem;
+        font-size: 1.1rem;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+      }
+
+      .grid {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+
+        max-width: 800px;
+        margin-top: 3rem;
+      }
+
+      .card {
+        margin: 1rem;
+        flex-basis: 45%;
+        padding: 1.5rem;
+        text-align: left;
+        color: inherit;
+        text-decoration: none;
+        border: 1px solid #333333;
+        border-radius: 10px;
+        transition: color 0.15s ease, border-color 0.15s ease;
+      }
+
+      .card:hover,
+      .card:focus,
+      .card:active {
+        color: #007dfa;
+        border-color: #007dfa;
+      }
+
+      .card h3 {
+        margin: 0 0 1rem 0;
+        font-size: 1.5rem;
+      }
+
+      .card p {
+        margin: 0;
+        font-size: 1.0rem;
+        line-height: 1.5;
+      }
+
+      @media (max-width: 600px) {
+        .grid {
+          width: 100%;
+          flex-direction: column;
+        }
+      }
+    `}</style>
+
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </div>
+)
+
+export default Home
