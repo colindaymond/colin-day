be4dc4e511d5e0e23d21eafadb833bb0b081da4d
c01in <5925270+colindaymond@users.noreply.github.com>
2020-06-05 08:43:40 +0200

    grab bag and link

--- pages/grab-bag.js
+++ pages/grab-bag.js
@@ -2,69 +2,47 @@ import Link from 'next/link'
 
 import Head from 'next/head'
 
-const Home = () => (
+export default function grabbag() {
+  return (
+
+
   <div className="container">
     <Head>
       <title>c01in</title>
       <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/237/call-me-hand_emoji-modifier-fitzpatrick-type-3_1f919-1f3fc_1f3fc.png"></link>
     </Head>
 
-    <main>
-      <h2 className="title">
-        c01in
-      </h2>
 
+    <main>
       <p className="description">
-        words and pictures
+        grab bag
       </p>
 
-      <div className="grid">
-        <a href="https://www.venturing.io" target="_blank" className="card">
-          <h3>words</h3>
-          <p>words from my blog, venturing</p>
-        </a>
 
-        <a href="/verse" className="card">
-        <h3>verse</h3>
-        <p>
-          words, new arrangements
-        </p>
-        </a>
+<div className="container">
 
-        <a href="https://www.venturing.io/photographs/" target="_blank" className="card">
-          <h3>pictures</h3>
-          <p>souvenirs from the journey</p>
-        </a>
+  <p className="poem">
+  <b> this moment is a portal and a hole </b> <br />
+  <br />
+  These words were written by White Eagle from the Hopi on March 16, 2020. While the words were specifically written in response to the novel coronavirus, his message on how to stay hopeful and resist through joy and care is even more vital today:<br />
 
-        <a href="https://www.youtube.com/watch?v=z9mjK8DYE3A&t=213s" target="_blank"
-          className="card">
-          <h3>moving pictures</h3>
-          <p>on air: yangshuo, china, 2001</p>
-        </a>
+This moment humanity is going through can now be seen as a portal and as a hole. The decision to fall into the hole or go through the portal is up to you. If they repent of the problem and consume the news 24 hours a day, with little energy, nervous all the time, with pessimism, they will fall into the hole. But if you take this opportunity to look at yourself, rethink life and death, take care of yourself and others, you will cross the portal. Take care of your home, take care of your body. Connect with the middle body of your spiritual House. Connect to the egregor of your spiritual home. Body, house, medium body, spiritual house, all this is synonymous, that is to say the same. When you are taking care of one, you are taking care of everything else. Do not lose the spiritual dimension of this crisis, have the aspect of the eagle, which from above, sees the whole, sees more widely. There is a social demand in this crisis, but there is also a spiritual demand. The two go hand in hand. Without the social dimension, we fall into fanaticism. But without the spiritual dimension, we fall into pessimism and lack of meaning. You were prepared to go through this crisis. Take your toolbox and use all the tools at your disposal. Learn about resistance with indigenous and African peoples: we have always been and continue to be exterminated. But we still haven't stopped singing, dancing, lighting a fire and having fun. Don't feel guilty about being happy during this difficult time. You don't help at all by being sad and without energy. It helps if good things emanate from the Universe now. It is through joy that one resists. Also, when the storm passes, you will be very important in the reconstruction of this new world. You need to be well and strong. And, for that, there is no other way than to maintain a beautiful, happy and bright vibration. This has nothing to do with alienation. This is a resistance strategy. In shamanism, there is a rite of passage called the quest for vision. You spend a few days alone in the forest, without water, without food, without protection. When you go through this portal, you get a new vision of the world, because you have faced your fears, your difficulties ... This is what is asked of you. Let them take advantage of this time to perform their vision seeking rituals. What world do you want to build for yourself? For now, this is what you can do: serenity in the storm. Calm down and pray. Everyday. Establish a routine to meet the sacred every day. Good things emanate, what you emanate now is the most important thing. And sing, dance, resist through art, joy, faith and love.
+</div>
 
-        <a href="https://soundcloud.com/coldaymond/likes" target="_blank"
-          className="card">
-          <h3>tunes</h3>
-          <p>
-            music i like maybe you like too
-          </p>
-        </a>
+<h2>
+<Link href="/">
+<a>
+&#127962;&#65039;
+</a>
+</Link>
+</h2>
 
-        <a href="/grab-bag"
-          className="card">
-          <h3>grab bag</h3>
-          <p>
-            random makes me smile
-          </p>
-        </a>
-
-      </div>
+    <footer>
+    &#129305;&#127996;
+    </footer>
 
     </main>
 
-    <footer>
-        &#129305;&#127996;
-    </footer>
 
     <style jsx>{`
       .poem {
@@ -93,7 +71,7 @@ const Home = () => (
       }
 
       main {
-        padding: 2rem 0;
+        padding: 5rem 0;
         flex: 1;
         display: flex;
         flex-direction: column;
@@ -104,14 +82,14 @@ const Home = () => (
       footer {
         width: 100%;
         height: 100px;
-        border-top: 1px solid #ffc2f7;
+        border-top: 1px solid #ff00e7;
         display: flex;
         justify-content: center;
         align-items: center;
       }
 
       footer img {
-        margin-left: 0rem;
+        margin-left: 0.5rem;
       }
 
       footer a {
@@ -126,7 +104,7 @@ const Home = () => (
       }
 
       .title a {
-        color: #ffc2f7;
+        color: #ff00e7;
         text-decoration: none;
       }
 
@@ -137,10 +115,10 @@ const Home = () => (
       }
 
       .title {
-        color: #ffc2f7;
+        color: #ff00e7;
         margin: 0;
         line-height: 1.15;
-        font-size: 3.5rem;
+        font-size: 3rem;
         font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
           DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
       }
@@ -152,7 +130,7 @@ const Home = () => (
 
       .description {
         line-height: 1.5;
-        font-size: 0.9rem;
+        font-size: 1.1rem;
         font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
           DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
       }
@@ -166,6 +144,10 @@ const Home = () => (
           DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
       }
 
+      .h2 {
+        text-align: right;
+      }
+
       .grid {
         display: flex;
         align-items: center;
@@ -173,17 +155,17 @@ const Home = () => (
         flex-wrap: wrap;
 
         max-width: 800px;
-        margin-top: 2rem;
+        margin-top: 3rem;
       }
 
       .card {
         margin: 1rem;
         flex-basis: 45%;
         padding: 1.5rem;
-        text-align: center;
+        text-align: left;
         color: inherit;
         text-decoration: none;
-        border: 0px solid #333333;
+        border: 1px solid #333333;
         border-radius: 10px;
         transition: color 0.15s ease, border-color 0.15s ease;
       }
@@ -191,15 +173,13 @@ const Home = () => (
       .card:hover,
       .card:focus,
       .card:active {
-        color: #7EA6F8;
-        border-color: #7EA6F8;
+        color: #007dfa;
+        border-color: #007dfa;
       }
 
       .card h3 {
         margin: 0 0 1rem 0;
-        font-size: 1.25rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        font-size: 1.5rem;
       }
 
       .card p {
@@ -230,7 +210,8 @@ const Home = () => (
         box-sizing: border-box;
       }
     `}</style>
-  </div>
-)
 
-export default Home
+</div>
+
+  )
+}
