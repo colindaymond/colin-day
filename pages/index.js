23f4845fcba81730635e76f7fbcd04df3f981ad7
c01in <5925270+colindaymond@users.noreply.github.com>
2021-02-10 21:07:23 +0100

    next/image.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 import Head from 'next/head'
 
-import Image from 'next/image'
+import Image from 'next/image.js'
 
 const Home = () => (
   <div className="container">
