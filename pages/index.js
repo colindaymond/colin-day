22578ab8ea7f1daa2ffc20d980b19d149cfc55de
c01in <5925270+colindaymond@users.noreply.github.com>
2021-02-11 12:27:14 +0100

    unanchor footer

--- pages/index.js
+++ pages/index.js
@@ -100,8 +100,6 @@ const Home = () => (
       }
 
       footer {
-        position: absolute;
-        bottom: 0;
         width: 100%;
         height: 50px;
         border-top: 1px solid #ffc2f7;
