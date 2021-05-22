f866e6f45118507c2059e4702bbde1e5f5a2088c
c01in <5925270+colindaymond@users.noreply.github.com>
2021-05-22 23:57:07 +0200

    try again

--- pages/josben.js
+++ pages/josben.js
@@ -31,7 +31,180 @@ Colin, Concetta and Oscar
 
 </p>
 
+<style jsx>{`
+  .poem {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-size: 1.0rem;
+    text-align: left;
+    padding: 2rem;
+  }
 
+  .letter {
+    font-family: EB Garamond;
+    color: #484848;
+
+  }
+
+  .poem-title {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-size: 1.0rem;
+    text-align: left;
+    font-weight: bold;
+  }
+
+  .container {
+    min-height: 0vh;
+    padding-top: 2.5rem;
+    padding-bottom: 3rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+  }
+
+  main {
+    padding: 0 rem;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer {
+    width: 100%;
+    height: 50px;
+    border-top: 1px solid #ffc2f7;
+    display: flex;
+    justify-content: center;
+    align-items: bottom;
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+      DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+  }
+
+  footer img {
+    margin-bottom: 0rem;
+  }
+
+  footer a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    color: #7EA6F8
+  }
+
+  .title a {
+    color: #ffc2f7;
+    text-decoration: none;
+  }
+
+  .title a:hover,
+  .title a:focus,
+  .title a:active {
+    text-decoration: underline;
+  }
+
+  .title {
+    color: #ffc2f7;
+    margin: 0;
+    line-height: 1.15;
+    font-size: 3.5rem;
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+      DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+  }
+
+  .title,
+  .description {
+    text-align: center;
+  }
+
+  .description {
+    line-height: 1.5;
+    font-size: 0.9rem;
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+      DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+  }
+
+  code {
+    background: #fafafa;
+    border-radius: 5px;
+    padding: 0.75rem;
+    font-size: 1.1rem;
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+      DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+  }
+
+  .grid {
+    display: flex;
+    align-items: left;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    max-width: 800px;
+    margin-top: 1rem;
+  }
+
+  .card {
+    margin: 0rem;
+    flex-basis: 45%;
+    padding: 0rem;
+    text-align: left;
+    color: inherit;
+    text-decoration: none;
+    border: 0px solid #333333;
+    border-radius: 10px;
+    transition: color 0.15s ease, border-color 0.15s ease;
+  }
+
+  .card:hover,
+  .card:focus,
+  .card:active {
+    color: #ffc2f7;
+    border-color: #ffc2f7;
+  }
+
+  .card h2 {
+    margin: 0 0 0.5rem 0;
+    font-size: 1rem;
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+      DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+  }
+
+  .card p {
+    margin: 0;
+    font-size: 1.0rem;
+    line-height: 1.5;
+  }
+
+  @media (max-width: 600px) {
+    .grid {
+      width: 100%;
+      flex-direction: column;
+    }
+  }
+`}</style>
+
+<style jsx global>{`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`}</style>
 
 
 )
