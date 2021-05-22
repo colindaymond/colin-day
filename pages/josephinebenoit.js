ef416a23733f2e9f5ce2869012976bb2c57f8148
c01in <5925270+colindaymond@users.noreply.github.com>
2021-05-22 23:30:27 +0200

    better breaks

--- pages/josephinebenoit.js
+++ pages/josephinebenoit.js
@@ -13,18 +13,18 @@ export default function photographs() {
 <p className="poem">
 
 
-Dear Benoit, Dear Josephine, <br /><br/>
-
+Dear Benoit, Dear Josephine, <br />
+<br/>
 When I heard the news that the two of you have agreed to try and spend a life together something within me clicked and
 fell together. Ben, you are an extraordinary man with a warm heart and a viking spirit. Josephine, you have tamed the untameable bull
-with your smile and because perhaps you yourself have more than a little wild. <br /><br/>
-
-The days with you both outside of Berlin were sun-drenched and wild. I can’t wait to share a moment again. <br /><br/>
-
-“You’ll never…. meet somebody, like you… meet me” <br /><br/>
-
-Love to you both <br /><br/>
-
+with your smile and because perhaps you yourself have more than a little wild. <br />
+<br/>
+The days with you both outside of Berlin were sun-drenched and wild. I can’t wait to share a moment again. <br />
+<br/>
+“You’ll never…. meet somebody, like you… meet me” <br />
+<br/>
+Love to you both <br />
+<br/>
 Colin
 
 </p>
